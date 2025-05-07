ddfb1c89f98e027be1e231616acd039f446674d8
admin <adithyavinod1943@gmail.com>
2025-05-08 01:00:37 +0530

    created uservalidationservice - api req using webclient

--- userservice/src/main/java/com/queue_management/userservice/controller/UserController.java
+++ userservice/src/main/java/com/queue_management/userservice/controller/UserController.java
@@ -25,7 +25,7 @@ public class UserController {
     }
 
     @GetMapping("/{userId}/validate")
-    public ResponseEntity<Boolean> getUserProfile(@PathVariable String userId){
+    public ResponseEntity<Boolean> validateUser(@PathVariable String userId){
         return ResponseEntity.ok(userService.existByUserId(userId));
     }
 
