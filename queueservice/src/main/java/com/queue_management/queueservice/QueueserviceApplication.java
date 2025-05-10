9a37fc228456359774b61ff22fad9ed18aa48ba6
admin <adithyavinod1943@gmail.com>
2025-05-10 23:44:21 +0530

    initialised queue-service

---
+++ queueservice/src/main/java/com/queue_management/queueservice/QueueserviceApplication.java
@@ -0,0 +1,13 @@
+package com.queue_management.queueservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueueserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueueserviceApplication.class, args);
+	}
+
+}
---
+++ queueservice/src/test/java/com/queue_management/queueservice/QueueserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.queue_management.queueservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueueserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
