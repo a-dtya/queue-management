45c4231eaecdb67848b0aae5aa90eb937213e447
admin <adithyavinod1943@gmail.com>
2025-05-11 00:06:56 +0530

    added queue-entry model

---
+++ queueservice/src/main/java/com/queue_management/queueservice/model/QueueEntry.java
@@ -0,0 +1,28 @@
+package com.queue_management.queueservice.model;
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "queue_entries")
+public class QueueEntry {
+
+    @Id
+    private String id;
+
+    private String userId;      // ID of the user joining the queue
+    private String shopId;      // ID of the shop the user is queuing at
+    private String serviceName; // Name of the service (e.g., "Haircut")
+
+    private int position;       // Position in the queue
+
+    private LocalDateTime joinedAt;   // Time when user joined the queue
+    private LocalDateTime estimatedServiceTime; // Optional: estimated service time
+    private LocalDateTime completedAt;          // If the service is completed
+}
